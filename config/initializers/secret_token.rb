b62ab304470c996ef9ff54f8d40565b7cf0beb0d
GrimmOutlook <grimmoutlook87@gmail.com>
2014-03-21 18:58:20 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Feedscraper::Application.config.secret_key_base = '28f765b0b7d12347db1be7d10c0cd4d4619f98ae01b54084d1980ed7ec34fe4f44cf56be7b7e4a2c61f4c245a7e266fcff92b4b6b667b97b2644cfec94272bb1'
---
+++ lib/tasks/scraper.rake
@@ -0,0 +1,6 @@
+namespace :scraper do
+  desc "Scrape Blog Feed of Some Websites"
+  task scrape: :environment do
+  end
+
+end
